81c82c56657b35885c160f9b3e489da8bb687578
huzidev <huzaifa.iqdev@gmail.com>
2023-03-29 03:00:52 +0500

    date logger

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dayjs = require("dayjs");
 function main() {
     try {
         // mmm for alphabetical month, mm for months in number hh:mmA, A for AM, PM
-        const format = "YYYY-MMM-DD, ddd hh:mmA"
+        const format = "YYYY-MMM-DD, ddd hh:mmA";
         const dateFormat = dayjs().format(format);
         console.log(dateFormat);
         fs.writeFileSync('date-log.txt', dateFormat);
